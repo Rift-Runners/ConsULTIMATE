5ecff3dccb00303cbb2fa0e466fb5e59ee729752
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-04-01 19:09:29 -0300

    Refactored whole header loading (1000x better)

--- web/resources/js/header-loader.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $("#header").load("templates/header.html");
-    $("#headerCliente").load("templates/headerCliente.html");
-    $("#headerConsultor").load("templates/headerConsultor.html");
-    $("#headerAdmin").load("templates/headerAdmin.html");
-});
-
