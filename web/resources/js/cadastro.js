55ad92d551ab0890860b86afda9a5a2d8f5eef5b
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-04-14 19:38:42 -0300

    Modificações na animação do cadastro

--- web/resources/js/cadastro.js
+++ web/resources/js/cadastro.js
@@ -2,14 +2,14 @@ $(function () {
     $(".perfil-cliente").click(function () {
         $(this).addClass("btn-primary");
         $(this).closest("body").find(".perfil-consultor").removeClass("btn-primary");
-        $(this).closest("body").find("#formulario-cliente").show();
-        $(this).closest("body").find("#formulario-consultor").hide();
+        $(this).closest("body").find("#formulario-cliente").slideDown();
+        $(this).closest("body").find("#formulario-consultor").slideUp();
     });
     $(".perfil-consultor").click(function () {
         $(this).addClass("btn-primary");
         $(this).closest("body").find(".perfil-cliente").removeClass("btn-primary");
-        $(this).closest("body").find("#formulario-consultor").show();
-        $(this).closest("body").find("#formulario-cliente").hide();
+        $(this).closest("body").find("#formulario-consultor").slideDown();
+        $(this).closest("body").find("#formulario-cliente").slideUp();
     });
 });
 
