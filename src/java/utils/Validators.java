d8a30f586fa6d1b22a43ce393f18db5f3c5ba0b9
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-04-13 23:51:14 -0300

    Validações de campos do cadastro dos usuários

--- src/java/utils/Validators.java
+++ src/java/utils/Validators.java
@@ -42,22 +42,68 @@ public class Validators {
         return (senhaInput.length() > 5 || senhaInput.length() < 17);
     }
 
-    public boolean validaCliente(Cliente cli) {
-        if (validaEmail(cli.getEmail())) {
-            if (validaUsuario(cli.getUsuario())) {
-                if (validaSenha(cli.getSenha())) {
-                    return true;
-                }
-            }
+    public boolean validaCpf(String cpfInput) {
+        if (cpfInput.length() != 14) {
+            return false;
         }
+        int[] posicoesMultiplicadasPrimeiro = new int[]{0, 0, 10, 9, 8, 6, 5, 4, 2, 1, 0};
+        int[] posicoesMultiplicadasSegundo = new int[]{0, 0, 12, 10, 9, 8, 6, 5, 4, 2, 1, 0};
+        int soma = 0;
+
+        for (int i = 10; i > 1; i--) {
+            soma += i * Integer.parseInt(cpfInput.substring(posicoesMultiplicadasPrimeiro[i], posicoesMultiplicadasPrimeiro[i] + 1));
+        }
+        int resultado = ((11 - (soma % 11)) == 10 || (11 - (soma % 11)) == 11) ? 0 : 11 - (soma % 11);
+        String parFinal = Integer.toString(resultado);
+        soma = 0;
+
+        for (int i = 10; i > 1; i--) {
+            soma += i * Integer.parseInt(cpfInput.substring(posicoesMultiplicadasSegundo[i], posicoesMultiplicadasSegundo[i] + 1));
+        }
+        resultado = ((11 - (soma % 11)) == 10 || (11 - (soma % 11)) == 11) ? 0 : 11 - (soma % 11);
+        parFinal += Integer.toString(resultado);
+
+        return (parFinal.equals(cpfInput.substring(12)));
+    }
+
+    public boolean validaRg(String rgInput) {
+        return (rgInput.length() != 10);
+    }
+
+    public boolean validaTelefone(String telInput) {
+        return (telInput.length() != 14 || telInput.length() != 15);
+    }
+    
+    public boolean validaCnpj(String telInput) {
+        return (telInput.length() != 17);
+    }
+
+    public boolean validaCliente(Cliente cli) {
+//        if (validaCpf(cli.getCpf())) {
+//            if (validaRg(cli.getRg())) {
+                if (validaTelefone(cli.getTelefone())) {
+                    if (validaEmail(cli.getEmail())) {
+                        if (validaUsuario(cli.getUsuario())) {
+                            if (validaSenha(cli.getSenha())) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+//            }
+//        }
         return false;
     }
 
     public boolean validaConsultor(Consultor cons) {
-        if (validaEmail(cons.getEmail())) {
-            if (validaUsuario(cons.getUsuario())) {
-                if (validaSenha(cons.getSenha())) {
-                    return true;
+        if (validaTelefone(cons.getTelefone())) {
+            if (validaCnpj(cons.getCnpj())) {
+                if (validaEmail(cons.getEmail())) {
+                    if (validaUsuario(cons.getUsuario())) {
+                        if (validaSenha(cons.getSenha())) {
+                            return true;
+                        }
+                    }
                 }
             }
         }
